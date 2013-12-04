08abf0fafdb3f5bef0182034abfa90531687b0db
laboon <laboon@gmail.com>
2013-12-04 14:47:07 -0500

    Added mandelbrot.  Requires gnuplot.

---
+++ mandelbrot.hs
@@ -0,0 +1,19 @@
+import Graphics.Gnuplot.Simple
+import Data.Complex
+
+mandelbrot :: Complex Double -> [Complex Double]
+mandelbrot n = iterate (\x -> x^2 + n) n
+
+shouldShow :: Complex Double -> Bool
+shouldShow x
+  | (magnitude ((mandelbrot x) !! 50)) < 2 = True
+  | otherwise = False
+
+allPoints :: [(Double, Double)]
+allPoints = [ (x,y) | x <- [-2, -1.995 .. 0.5], y <- [1, 0.995 .. -1]]
+
+mandelbrotPoints :: [(Double, Double)]
+mandelbrotPoints = filter (\p -> (shouldShow ((fst p) :+ (snd p)))) allPoints
+
+main = do
+  plotListStyle [] (defaultStyle{plotType = Dots}) mandelbrotPoints
